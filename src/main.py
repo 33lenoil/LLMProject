88cd31bd96275b52ed2716ab59f83c9b85bdb213
lionelhu <lionelhu33@gmail.com>
2023-05-03 00:04:06 -0500

    a simple translator

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
 
@@ -6,9 +7,13 @@ def main():
     llm = OpenAI(temperature=0.9)
 
     while 1:
-        curr_input = input("Ask me something.\n")
-        # curr_input = "What would be a good company name for a company that makes colorful socks?"
-        print(llm(curr_input))
+        country_prompt = PromptTemplate(
+            template="how to say \"{text}\" in {language}?",
+            input_variables=["text", "language"],
+        )
+        text_input = input("Please type in a piece of text.\n")
+        language_input = input("What language do you want to translate to?\n")
+        print(llm(country_prompt.format(text=text_input, language=language_input)))
 
 
 if __name__ == "__main__":
