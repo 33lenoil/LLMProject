601dc3647bf33df2bd856ea89678cd67d3f090d8
lionelhu <lionelhu33@gmail.com>
2023-05-02 23:51:50 -0500

    can now take an input and print a response

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ def main():
     print("Program started!")
     llm = OpenAI(temperature=0.9)
 
+    while 1:
+        curr_input = input("Ask me something.\n")
+        # curr_input = "What would be a good company name for a company that makes colorful socks?"
+        print(llm(curr_input))
+
 
 if __name__ == "__main__":
     main()
